c9432e5ee8d0e323286f0ea72a2382f5d08fb2c3
Ratul Anjum <mdratulanjum@gmail.com>
2024-06-01 12:56:23 +0600

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-const question = document.querySelector(".question");
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
 
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {
 
-question.addEventListener("click", (e) => {
-    console.log(e.target.parentElement.nextElementSibling);
-    // e.target.parentElement.nextElementSibling.style.display = "block";
+        //  Display only one collapsed item at a time!
+
+        const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+        if (currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader !== accordionItemHeader) {
+            currentlyActiveAccordionItemHeader.classList.toggle("active");
+            currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+        }
+
+        accordionItemHeader.classList.toggle("active");
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+        if (accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        }
+        else {
+            accordionItemBody.style.maxHeight = 0;
+        }
+
+    });
 });
